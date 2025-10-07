61fbb9026df877939bdd33787907d30f589004e3
Noor Fatima Qureshi <128814375+FatimaBadar@users.noreply.github.com>
2025-10-07 17:37:37 +1300

    Setup with all libraries, and dependencies installed

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { PrimeReactProvider } from 'primereact/api';
+import "primereact/resources/themes/md-light-deeppurple/theme.css";
+import 'primereact/resources/primereact.min.css';
+import "primeicons/primeicons.css"; //icons
+import 'primeflex/primeflex.css';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <PrimeReactProvider>
+    <App />
+    </PrimeReactProvider> 
+  </StrictMode>,
+)
