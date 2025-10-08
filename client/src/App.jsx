796b0989dc5cc2d4739019e7addad28e7a659032
Noor Fatima Qureshi <128814375+FatimaBadar@users.noreply.github.com>
2025-10-09 01:18:03 +1300

    Fixed minor error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,3 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -12,8 +9,6 @@ import Account from "./pages/Account";
 import DashboardComponent from "./components/Dashboard/DashboardComponent";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
       <BrowserRouter>
@@ -25,8 +20,6 @@ function App() {
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/account" element={<Account />} />
           <Route path="/account/dashboard" element={<DashboardComponent />} />
-          <Route path="/" element={<Home />} />
-
           {/* <Route path="/projects" element={<Projects />} /> */}
         </Routes>
       </BrowserRouter>
--- client/src/components/AboutUs/AboutusComponent.jsx
+++ client/src/components/AboutUs/AboutusComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './AboutUs.css';
+import './Aboutus.css';
 
 const AboutUsComponent = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
